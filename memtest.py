d3d0a1c4d7a7b0c4b4cefb0d5acf53f41419b267
Xudong Huang <xudongh@juniper.net>
2015-03-06 14:57:27 +0800

    simple interact with memtest86

--- memtest.py
+++ memtest.py
@@ -29,9 +29,7 @@ class roller (threading.Thread):
     """This runs a function in a loop in a thread."""
 
     def __init__(self, interval, function, args=[], kwargs={}):
-        """The interval parameter defines time between each call to the function.
-        """
-
+        """The interval parameter defines time between each call to the function."""
         threading.Thread.__init__(self)
         self.interval = interval
         self.function = function
@@ -40,8 +38,6 @@ class roller (threading.Thread):
         self.finished = threading.Event()
 
     def cancel(self):
-        """Stop the roller."""
-
         self.finished.set()
 
     def run(self):
@@ -49,17 +45,6 @@ class roller (threading.Thread):
         while not self.finished.isSet():
             # self.finished.wait(self.interval)
             self.function(*self.args, **self.kwargs)
-
-
-def endless_poll(child, screen):
-    """This keeps the screen updated with the output of the child. This runs in
-    a separate thread. See roller(). """
-
-    try:
-        s = child.read_nonblocking(4000, 0.1)
-        screen.write(s)
-    except:
-        pass
 
 
 class memtest86(object):
@@ -72,48 +57,102 @@ class memtest86(object):
         r, c = self.child.getwinsize()
         self.crt = ANSI.ANSI(r, c)
 
+    def _update_crt(self):
+        """This keeps the screen updated with the output of the child. This runs in
+        a separate thread. See roller(). """
+        try:
+            buf = self.child.read_nonblocking(4000, 0.1)
+            self.crt.write(buf)
+        except:
+            pass
+
     def start(self):
-        # if this is a duplicate session, choose 1 option to enter
+        """Start to monitor the remote memtest86"""
+        # if this is a duplicate session, choose option 1 to enter
         if 0 == self.child.expect(["Enter your option : ", "Time: "]):
             print "enter duplicated session"
             self.crt.write(self.child.before)
             self.crt.write(self.child.after)
             self.child.send("1")
 
-        self.roller = roller(0.1, endless_poll, (self.child, self.crt))
+        self.roller = roller(0.1, self._update_crt)
         self.roller.start()
         self.refresh()
-        print "memtest86 started"
-        print "----------------------------------------\n"
 
     def stop(self):
+        """Stop to monitor the remote memtest86"""
         if self.roller:
             self.roller.cancel()
 
         self.child.terminate()
-        print "\n----------------------------------------"
 
     def refresh(self):
+        """refresh the crt so that we can get full data"""
         self.child.send('c0')
+        while "DDR3" not in self.get_info():
+            time.sleep(0.1)
+
+    def restart(self):
+        """restart the memtest86 from beginning"""
+        self.child.send('c3s')
+        while "Test 0" not in self.get_current_test():
+            time.sleep(0.1)
+
+    def reboot(self):
+        """exit memtest86 and reboot the DUT"""
+        self.child.send('')
         time.sleep(1)
 
     def dump(self):
+        """dump the crt screen"""
         print str(self.crt)
 
+    def config(self):
+        """config the remote memtest86"""
+        pass
+
     def get_region(self, rs, cs, re, ce):
+        """retrieve the strings from crt screen region"""
         l = self.crt.get_region(rs, cs, re, ce)
-        s = [ i.strip() for i in l]
+        s = [i.strip() for i in l]
         return '\n'.join(s)
 
+    def get_version(self):
+        """retrieve the version string"""
+        return self.get_region(1, 1, 1, 80)
 
-    def print_sysinfo(self):
-        version = self.get_region(1, 1, 1, 80)
-        print version
-        mem_info = self.get_region(2, 1, 6, 28)
-        print mem_info
+    def get_info(self):
+        """retrieve the memory information"""
+        return "\n".join((self.get_region(2, 1, 6, 28),
+                          self.get_region(7, 1, 7, 80)))
 
+    def get_time(self):
+        """retrieve the total test running time"""
+        return self.get_region(12, 6, 12, 16)
 
-    
+    def get_errors(self):
+        """retrieve the total test errors"""
+        return self.get_region(12, 73, 12, 80)
+
+    def get_cpus(self):
+        """retrieve the total available cpu number"""
+        return self.get_region(9, 53, 9, 80)
+
+    def get_current_pass(self):
+        """retrieve the current test round"""
+        return self.get_region(12, 40, 12, 65)
+
+    def get_current_test(self):
+        """retrieve the current test item"""
+        return self.get_region(4, 30, 4, 80)
+
+    def get_test_progress(self):
+        """retrieve the current test progress"""
+        return self.get_region(3, 35, 3, 40)
+
+    def get_pass_progress(self):
+        """retrieve the current round test progress"""
+        return self.get_region(2, 30, 2, 80)
 
 
 def main():
@@ -140,11 +179,58 @@ def main():
     test = memtest86(hostname, port)
     try:
         test.start()
-        test.print_sysinfo()
-        test.dump()
+        print "memtest86 started"
+        print "----------------------------------------\n"
+        print test.get_version()
+        print "\nsystem memory information"
+        print test.get_info()
+
+        print "\nstart memory86 test"
+        test.restart()
+        old_round = ""
+        old_test = ""
+        old_progress = ""
+        while True:
+            # check errors
+            e = int(test.get_errors())
+            if e != 0:
+                print "error happened!"
+                break
+
+            # check test round
+            r = test.get_current_pass()
+            if r != old_round:
+                old_round = r
+                print "\r%s" % r
+
+            # check test item
+            t = test.get_current_test()
+            # check finish condition
+            if "Test 3" in t:
+                sys.stdout.write("\r")
+                break
+
+            if t != old_test:
+                old_test = t
+                print "\r%s" % t
+
+            # check test progress
+            p = test.get_test_progress()
+            if p != old_progress:
+                old_progress = p
+                sys.stdout.write("\r    \r%s" % test.get_test_progress())
+                sys.stdout.flush()
+
+            time.sleep(0.1)
     finally:
         test.stop()
-        print "done!"
+        print "----------------------------------------"
+        print "time used: %s\n" % test.get_time()
+        error = int(test.get_errors())
+        if error != 0:
+            print "test failed! Errors: %s" % test.get_errors()
+        else:
+            print "test passed"
 
 if __name__ == "__main__":
 
